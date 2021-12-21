aa66c8478ab3013f409a6aaada237406467bac22
Ishit Beswal <ib370@snu.edu.in>
2021-12-22 00:44:31 +0530

    update: Added YoutubeVideo model

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import YoutubeVideo
 
 # Register your models here.
+class YoutubeVideoAdmin (admin.ModelAdmin) :
+    list_display = ('video_id', 'title', 'published')
+
+admin.site.register(YoutubeVideo, YoutubeVideoAdmin)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class YoutubeVideo(models.Model) :
+    id              =   models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+    video_id        =   models.CharField(max_length = 500, null = True, blank = True)
+    title           =   models.CharField(max_length = 500, null = True, blank = True)
+    channel_id      =   models.CharField(max_length = 500, null = True, blank = True)
+    channel_title   =   models.CharField(max_length = 500, null = True, blank = True)
+    description     =   models.TextField(default = '', null = True, blank = True)
+    thumbnail       =   models.CharField(max_length = 800, null = True, blank = True)
+    published       =   models.DateTimeField()
+
+    class Meta :
+        db_table    =   'Videos'
+
+    def __str__(self) -> str:
+        return self.title
