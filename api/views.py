357b75e7df9f371f9adbc398c7a36bf613ba3948
Ishit Beswal <ib370@snu.edu.in>
2021-12-22 02:04:25 +0530

    update: Added query limit to get view for stored videos

--- api/views.py
+++ api/views.py
@@ -1,10 +1,19 @@
 from django.db.models import query
 from django.shortcuts import render
+from rest_framework.response import Response
 from .models import YoutubeVideo
 from .serializers import YoutubeVideoSerializer
 from rest_framework import generics
 
 # Create your views here.
 class YoutubeVideoView (generics.ListAPIView) :
-    queryset = YoutubeVideo.objects.all().order_by('published')
+    queryset = YoutubeVideo.objects.all().order_by('-published')
     serializer_class = YoutubeVideoSerializer
+
+    def list (self, request) :
+        # Default limit is 10
+        limit = int(request.query_params.get('limit') or '10')
+
+        queryset = self.get_queryset()[:limit]
+        serializer = YoutubeVideoSerializer(queryset, many=True)
+        return Response(serializer.data)
